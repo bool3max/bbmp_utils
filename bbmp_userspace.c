aa185e17ec090076b50f9efb418d9f0c1986e3c2
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2019-12-20 18:27:35 +0100

    Implemented counter-clockwise rotation in bbmp_rot90()

--- bbmp_userspace.c
+++ bbmp_userspace.c
@@ -9,22 +9,22 @@ bbmp_Image *bbmp_rot90(bbmp_Image *image, const enum clock_dir direction) {
      * Returns NULL on failure or the pointer to the bbmp_Image on success.
     */
 
-    if (image->metadata.pixelarray_width != image->metadata.pixelarray_height) return NULL;
+    if (image->metadata.pixelarray_width != image->metadata.pixelarray_height || (direction != CW && direction != CCW)) return NULL;
+
     const size_t r = image->metadata.pixelarray_width;
 
-    if (direction == CW) {
-            // in-place transposition
-            for(size_t n = 0; n <= r - 2; n++) {
-                for(size_t m = n + 1; m <= r - 1; m++) {
-                    bbmp_Pixel temp = image->pixelarray[n][m];
-                    
-                    image->pixelarray[n][m] = image->pixelarray[m][n];
-                    image->pixelarray[m][n] = temp;
-                }
-            }
-
-            // reverse the columns to achieve clockwise rotation
+    // in-place transposition
+    for(size_t n = 0; n <= r - 2; n++) {
+        for(size_t m = n + 1; m <= r - 1; m++) {
+            bbmp_Pixel temp = image->pixelarray[n][m];
             
+            image->pixelarray[n][m] = image->pixelarray[m][n];
+            image->pixelarray[m][n] = temp;
+        }
+    }
+
+    if (direction == CW) {
+            // reverse the columns to achieve clockwise rotation
             for(size_t i_col = 0; i_col < r; i_col++) {
                 size_t r_s = 0,
                        r_e = r - 1;
@@ -40,12 +40,24 @@ bbmp_Image *bbmp_rot90(bbmp_Image *image, const enum clock_dir direction) {
                 } while (r_s != (r / 2));
             }
     } else if (direction == CCW) {
-
-
-
         // reverse the rows to achieve counter-clockwise rotation
-        return NULL;
-    } else return NULL;
+        for(size_t i_row = 0; i_row < r; i_row++) {
+            size_t c_s = 0,
+                   c_e = r - 1;
 
-    return image;        
+            do {
+                bbmp_Pixel temp = image->pixelarray[i_row][c_s];
+
+                image->pixelarray[i_row][c_s] = image->pixelarray[i_row][c_e];
+                image->pixelarray[i_row][c_e] = temp;
+
+                c_s++;
+                c_e--;
+
+            } while (c_s != (r / 2));
+
+        }
+    }
+
+    return image;
 }
