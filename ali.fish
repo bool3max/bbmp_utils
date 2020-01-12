0a6f502f4ad6e54f9e0de36391cf50e8b0d77f91
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2020-01-12 20:32:46 +0100

    Python3 test alias

--- ali.fish
+++ ali.fish
@@ -2,9 +2,11 @@
 # to expose the following aliases for easier testing
 # b: build the debug build
 # rt: run the default test from the debug build
+# prt: run the python3 test executable with PYTHONPATH set accordingly
 # vrt: run valgrind with full leack checking on the test from the debug build
 
 abbr -ag b 'ninja -C build_dbg'
 abbr -ag rt './build_dbg/tests/bbmp_utils_test'
+abbr -ag prt 'env PYTHONPATH=./build_dbg python3 ./tests/mod.py'
 abbr -ag vrt 'valgrind --leak-check=full ./build_dbg/tests/bbmp_utils_test'
 abbr -ag g 'gdb ./build_dbg/tests/bbmp_utils_test'
